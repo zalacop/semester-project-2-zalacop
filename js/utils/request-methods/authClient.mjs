4a7270018d9ac6ce2304ed99ab806564016ddaa0
zalacop <103320914+zalacop@users.noreply.github.com>
2023-12-06 22:38:45 +0100

    adds login user

--- js/utils/request-methods/authClient.mjs
+++ js/utils/request-methods/authClient.mjs
@@ -19,3 +19,38 @@ export async function registerUser(userRegisterData) {
     }
 }
 
+export async function loginUser(email, password) {
+    try {
+        const loginInfo = {
+            email: email,
+            password: password
+        }
+
+        const loginData = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(loginInfo),
+        }
+
+        const response = await fetch(urlLogin, loginData);
+     
+        if(response.ok) {
+            const data = await response.json();
+            localStorage.setItem('accessToken', data.accessToken);
+            localStorage.setItem('name', data.name);
+            localStorage.setItem('email', data.email);
+            localStorage.setItem('avatar', data.avatar)
+
+            return data;
+
+        } else {
+            return null;
+        }
+
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}

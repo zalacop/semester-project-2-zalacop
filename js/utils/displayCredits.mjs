050e2d553c4e024db7bc9fb42e0e6aaf149db6f3
zalacop <103320914+zalacop@users.noreply.github.com>
2023-12-16 00:21:07 +0100

    fixed credits

--- js/utils/displayCredits.mjs
+++ js/utils/displayCredits.mjs
@@ -1,14 +1,14 @@
+const creditContainer = document.querySelector(".credits");
 const showCredits = document.querySelector("#total-credits");
 
 export default function displayCredits() {
     const isThereToken = localStorage.getItem('accessToken');
 
-    if(isThereToken) {
+    if(!isThereToken) {
+        return creditContainer.innerHTML = "";
+    } else {
         const getCredits = localStorage.getItem('credits');
         showCredits.innerText = getCredits;
         return showCredits;
-
-    } else {
-        return showCredits.innerHTML = "";
     }
 }
